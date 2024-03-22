a6b6466f77371da479a742f5b3e065bc7c499641
Jan-Phillip Oesterling <jpo@hav.media>
2024-03-22 16:24:06 +0100

    feature/bette-compatibility (#19)
    * Add better version compatibility and testing for more versions
    
    * Add better version compatibility and testing for more versions

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name="aura_maintainer",
-    version="0.1",
-    python_requires='>=3.11',
+    version="0.2",
+    python_requires='>=3.9',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
